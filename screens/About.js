26e3a7139b62d2067a3f28d19152e5a08b141b8c
Jaden Arceneaux <arceneauxjaden@gmail.com>
2020-05-05 22:27:28 -0700

    working on list view

--- screens/About.js
+++ screens/About.js
@@ -1,13 +1,48 @@
-import { Ionicons } from '@expo/vector-icons';
-import * as WebBrowser from 'expo-web-browser';
+
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Linking, FlatList} from 'react-native';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 
-export default function LinksScreen() {
+
+
+
+const DATA = [
+    {
+        title: 'This App',
+        info: 'An app about me',
+        link: 'https://expo.io/@jaden-arceneaux/jadens-app'
+    },
+    {
+        title: 'Scripty',
+        info: 'A text editor for ChromeOS',
+        link: 'https://jadens-arc.github.io/Scripty/'
+    },
+    {
+        title: 'Ubuntu Brightness Control',
+        info: 'A small application to adjust brightness on ubuntu desktop',
+        link: 'https://github.com/Jadens-arc/Ubuntu-Brightness'
+    },
+]
+
+
+function Item({ title, info, link }) {
+    return (
+        <View style={styles.item} onPress={() => Linking.openURL(link)}>
+            <Text style={styles.title} onPress={() => Linking.openURL(link)}>{title}</Text>
+            <Text onPress={() => Linking.openURL(link)}>{info}</Text>
+        </View>
+    );
+}
+
+
+export default function About() {
     return (
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-
+            <FlatList
+                data={DATA}
+                renderItem={({ item }) => <Item title={item.title} info={item.info} link={item.link} />}
+            />
+            
         </ScrollView>
     );
 }
@@ -17,6 +52,15 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fafafa',
+    },
+    item: {
+        backgroundColor: '#eeeeee',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
     },
 
 });
