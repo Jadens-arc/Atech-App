983da814c3425c84047583b9879625dd786fa2fb
Jaden Arceneaux <arceneauxjaden@gmail.com>
2020-05-05 22:15:56 -0700

    added mav pulse web view

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,16 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { StyleSheet,  View } from 'react-native';
 
-import { MonoText } from '../components/StyledText';
+import { WebView } from 'react-native-webview';
+
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-
+      <WebView
+        source={{ uri: 'https://mavpulse.com/' }}
+      />
     </View>
   );
 }
